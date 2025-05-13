e97236f3c47f5250215eae73e9be4bd4402673fe
Poggii <s8901744h@majorana-giorgi.edu.it>
2025-05-13 09:47:50 +0200

    2. update movimento
    inizializzazione del robot e inserimento del movimento del robot

---
+++ test/src/test/GuidaTest.java
@@ -0,0 +1,23 @@
+package test;
+import lejos.hardware.Button;
+
+public class GuidaTest extends Test {
+    /**
+     * Main function of program.
+     */
+    public static void main(String[] args) {
+        initRobot();
+        initPilot();
+
+        System.out.println("Press any button to start!");
+        Button.waitForAnyPress();
+            
+        // Add your code below here
+        
+        // Drive in a 0.5 meter square
+        for (int i = 0; i < 4; i++) {
+            pilot.travel(0.5);
+            pilot.rotate(90);
+        }
+    }
+}
